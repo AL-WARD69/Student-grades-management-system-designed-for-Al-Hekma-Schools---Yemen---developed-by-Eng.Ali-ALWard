02598465346f9956c52ab14b7e27175bb97a8a24
Ali AL-Ward <aliabdullahalward@gmail.com>
2024-12-01 18:45:03 +0300

    Add files via upload

---
+++ includes/leftbar.php
@@ -0,0 +1,71 @@
+<link rel="stylesheet" href="css/bootstrap-rtl.min.css" media="screen" >
+
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="John Doe" class="img-circle profile-img">
+                                <h6 class="title">Admin</h6>
+                                <small class="info">مدير النظام</small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">الفئات الرئيسية</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>الرئيسية</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">فئات النظام</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>فصول الطلاب</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><i class="fa fa-bars"></i> <span>انشاء فصل دراسي</span></a></li>
+                                            <li><a href="manage-classes.php"><i class="fa fa fa-server"></i> <span>ادراة الفصول الدارسية</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+  <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>المواد الدراسية</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"><i class="fa fa-bars"></i> <span>اضافة مادة</span></a></li>
+                                            <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>عرض وادارة المواد الدراسية</span></a></li>
+                                           <li><a href="add-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>اضافة مادة للفصول الدارسية </span></a></li>
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>الطلاب</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>اضافة طالب</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>عرض وادارة الطلاب</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>النتائج</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-result.php"><i class="fa fa-bars"></i> <span>اضافة نتيجة</span></a></li>
+                                            <li><a href="manage-results.php"><i class="fa fa fa-server"></i> <span>عرض وادارة النتائج</span></a></li>
+                                           
+                                        </ul>        </li>
+
+
+<li class="has-children">
+                                       
+
+
+
+                                        <li><a href="change-password.php"><i class="fa fa fa-server"></i> <span> تغيير كلمة المرور</span></a></li>
+                                        <li><a href="index.php"><i class="fa fa fa-server"></i> <span> تسجيل الخروج</span></a></li>
+
+                            
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+error_reporting(0);
+include('includes/config.php'); 
+?>
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>مدارس اجيال الحكمة </title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body dir="rtl">
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark"style="float:right">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">(مدارس اجيال الحكمة-اليمن -صنعاء)</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a  class="nav-link active" aria-current="page" href="#!">الرئيسة</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="find-result.php">الطالب</a></li>
+                        <!-- li class="nav-item"><a class="nav-link active" href="admin-login.php">مدير النظام</a></li-->
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header - set the background image for the header in the line below-->
+        <header class="py-5 bg-image-full" style="background-image: url('images/background-image.jpg')">
+    
+        </header>
+        <!-- Content section-->
+        <section class="py-5">
+            <div class="container my-5" >
+                <div class="row justify-content-center">
+                    <div class="col-lg-6" >
+                       
+
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white"> تم تطوير النظام بواسطة ورد للحلول البرمجية والانظمة المتكاملة</p></div>
+            <div class="container"><p class="m-0 text-center text-white"> م:علي الورد --للتواصل/774087409</p></div>
+
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
